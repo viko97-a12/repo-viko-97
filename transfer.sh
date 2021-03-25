7813634825a2d0afc36f54da6f168c6efc55fbe1
viko97-a12 <viktor.milev@abv.bg>
2021-03-25 13:21:58 +0100

    deeee

--- transfer.sh
+++ transfer.sh
@@ -5,7 +5,15 @@
 
 git pull
 
+
+#1.5 Time when the transfer is performed!
+echo "The deployment from GoToAdmin has been performed on: - " >> business-tracking-date
+date >> business-tracking-date
+
 #2.Transfer it to server2
 
 scp /root/viko/remote_repo/* root@157.90.228.79:~/root
+
+
+
 
